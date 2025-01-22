ff47cc0244691f19f52243ff8238a74234e18774
Bilal-motiwala <bilalmotiwala20@gmail.com>
2025-01-22 19:51:33 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Footer from "@/Components/Footer";
 import Header from "@/Components/Header";
 import Products from "@/Components/Products";
+import "./globals.css";
 
 export default function Home() {
   return (
